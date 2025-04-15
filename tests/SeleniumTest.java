5bfab875ea81c3b5f0420df8e752f2c4bac2cccf
akash <akashsuthar5791@gmail.com>
2025-04-15 05:51:50 +0000

    sec Commint

--- tests/SeleniumTest.java
+++ tests/SeleniumTest.java
@@ -1,21 +1,36 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTest {
     public static void main(String[] args) {
+        // Set the ChromeDriver path
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        WebDriver driver = new ChromeDriver();
+
+        // Configure Chrome to run in headless mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        // Create a new ChromeDriver instance with options
+        WebDriver driver = new ChromeDriver(options);
 
         try {
             driver.get("http://localhost");
-            driver.findElement(By.linkText("About")).click();
-            String text = driver.findElement(By.id("about")).getText();
+
+            WebElement aboutLink = driver.findElement(By.linkText("About"));
+            aboutLink.click();
+
+            WebElement aboutText = driver.findElement(By.id("about"));
+            String text = aboutText.getText();
 
             if (text.contains("This is About page")) {
                 System.out.println("TEST PASSED");
             } else {
-                System.out.println("TEST FAILED: Unexpected content");
+                System.out.println("TEST FAILED: Text not matched");
                 System.exit(1);
             }
         } catch (Exception e) {
